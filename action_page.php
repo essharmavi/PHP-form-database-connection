a829726ee08d3be64b740212b2e7a3dbe2a8365c
essharmavi <38400230+essharmavi@users.noreply.github.com>
2020-04-10 22:09:19 +0530

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,24 +1,26 @@
 <?php
-$username=$_POST['username'];
-$email=$_POST['email'];
-$password=$_POST['psw'];
+	// Show PHP version
+	echo "Current PHP version: " . phpversion() .  "\n";
+
+	// Connect to database
+	$conn = mysqli_connect("localhost","vishal","vishal","useraccounts") or die(" Connection Error " . mysqli_error($conn));
+	$username=$_POST['username'];
+	$email=$_POST['email'];
+
+	$sql="INSERT INTO users(username,email) VALUES ('$username','$email')";
+
+	$result = mysqli_query($conn, $sql) or die("Insertion Error " . mysqli_error($conn));
+
+echo "Values Inserted";
 
 
 
-$conn= new mysqli('localhost','root','','useraccounts');
 
 
-if ($conn->connect_error) {
-	die("Error " . $conn->connect_error);
-}
-else{
-	$stmt=$conn->prepare("insert into users(username,email,password)values(?,?,?,?)");
-	$stmt->ind_param('sss', $username,$email,$password);
-	$stmt.exec();
-	echo "registration successful";
-	$stmt.close()
-	$conn.close()
-}
+
 ?>
+
+
+
 
 
